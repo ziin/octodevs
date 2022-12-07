eb08260b98af939d93fbbac2dae3420fdc43c24c
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2022-12-07 11:22:18 -0400

    remove unnecessary code

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,6 @@
 import { router } from "../trpc";
-import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 
-export const appRouter = router({
-  example: exampleRouter,
-  auth: authRouter,
-});
+export const appRouter = router({});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
