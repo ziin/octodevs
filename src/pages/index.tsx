6cc1fbab66484c7bf7d74d72235599af0f77c34e
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2023-01-04 16:01:02 -0400

    add contribute link in the upper right corner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { UserInfo } from "../components/UserInfo";
 import { createContextInner } from "../server/trpc/context";
 import { appRouter } from "../server/trpc/router/_app";
 import { trpc } from "../utils/trpc";
+import { MarkGithubIcon } from "@primer/octicons-react";
 
 const Home: NextPage = () => {
   const { data: profiles } = trpc.profile.getMany.useQuery();
@@ -24,6 +25,18 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {/* Contribute button */}
+      <div className="absolute top-0 right-0 inline-block w-14 overflow-hidden">
+        <a
+          className="flex h-20 origin-top-left -rotate-45 transform items-center bg-[#0c0d0e] p-1 text-gray-300 hover:text-gray-200"
+          target="_blank"
+          href="https://github.com/ziin/octodevs"
+          rel="noreferrer"
+        >
+          <MarkGithubIcon className="rotate-90 transform" size={24} />
+        </a>
+      </div>
 
       <Header />
 
