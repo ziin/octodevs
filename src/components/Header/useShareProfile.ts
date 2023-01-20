95c9d20979995afd40ded968053c83955116a55e
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2023-01-20 12:40:28 -0400

    fix publishing profile

--- src/components/Header/useShareProfile.ts
+++ src/components/Header/useShareProfile.ts
@@ -7,22 +7,6 @@ export function useShareProfile() {
 
   const { mutate: publish, isLoading: isPublishing } =
     trpc.profile.publish.useMutation({
-      onMutate: () => {
-        utils.profile.getPaginated.cancel();
-        utils.profile.me.cancel();
-
-        const prev = utils.profile.getPaginated.getData();
-
-        if (prev !== undefined && profile) {
-          const newProfile = { ...profile, published: true };
-          utils.profile.me.setData(undefined, newProfile);
-        }
-
-        return { prev };
-      },
-      onError: () => {
-        utils.profile.me.setData(undefined, profile);
-      },
       onSettled: () => {
         utils.profile.me.invalidate();
         utils.profile.getPaginated.invalidate();
@@ -30,21 +14,6 @@ export function useShareProfile() {
     });
   const { mutate: unpublish, isLoading: isUnpublishing } =
     trpc.profile.unpublish.useMutation({
-      onMutate: () => {
-        utils.profile.getPaginated.cancel();
-        utils.profile.me.cancel();
-
-        const prev = utils.profile.getPaginated.getData();
-
-        if (prev !== undefined && profile) {
-          utils.profile.me.setData(undefined, { ...profile, published: false });
-        }
-
-        return { prev };
-      },
-      onError: () => {
-        utils.profile.me.setData(undefined, profile);
-      },
       onSettled: () => {
         utils.profile.me.invalidate();
         utils.profile.getPaginated.invalidate();
--- src/server/trpc/router/profile.ts
+++ src/server/trpc/router/profile.ts
@@ -1,15 +1,14 @@
 import { TRPCError } from "@trpc/server";
 import { router, protectedProcedure, publicProcedure } from "../trpc";
 import z from "zod";
+import type { UserResponse } from "../../../types/github";
+import { mapGithubUserToProfile } from "../../../utils/functions";
+import type { Context } from "../context";
 
 export const profileRouter = router({
   me: protectedProcedure.query(async ({ ctx }) => {
     try {
-      return await ctx.prisma.profile.findFirst({
-        where: {
-          userId: ctx.session.user.id,
-        },
-      });
+      return await getMe(ctx);
     } catch (error) {
       throw new TRPCError({
         code: "INTERNAL_SERVER_ERROR",
@@ -58,6 +57,23 @@ export const profileRouter = router({
 
   publish: protectedProcedure.mutation(async ({ ctx }) => {
     try {
+      // check if profiles exists
+      const profile = await getMe(ctx);
+
+      // if not, fetch on github
+      if (!profile) {
+        const user = await fetchMyGithubProfile(ctx);
+
+        await ctx.prisma.profile.create({
+          data: {
+            userId: ctx.session.user.id,
+            published: true,
+            ...mapGithubUserToProfile(user),
+          },
+        });
+      }
+
+      // update profile with published true
       await ctx.prisma.profile.update({
         data: { published: true },
         where: { userId: ctx.session?.user?.id },
@@ -84,3 +100,45 @@ export const profileRouter = router({
     }
   }),
 });
+
+async function fetchMyGithubProfile(ctx: Context) {
+  const token = await getGithubToken(ctx);
+  const user = await fetchGithubUserWithToken(token);
+  if (!user) throw new Error("Error fetching github user");
+  return user;
+}
+
+async function getGithubToken(ctx: Context) {
+  const { access_token: token } = await ctx.prisma.account.findFirstOrThrow({
+    where: {
+      userId: ctx.session?.user?.id,
+    },
+  });
+  if (!token) throw new Error("Token not found");
+  return token;
+}
+
+async function getMe(ctx: Context) {
+  return await ctx.prisma.profile.findFirst({
+    where: {
+      userId: ctx.session?.user?.id,
+    },
+  });
+}
+
+async function fetchGithubUserWithToken(
+  token: string,
+): Promise<UserResponse | null> {
+  try {
+    const response = await fetch("https://api.github.com/user", {
+      headers: {
+        accept: "application/vnd.github+json",
+        authorization: `Bearer ${token}`,
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+    return await response.json();
+  } catch (error) {
+    return null;
+  }
+}
